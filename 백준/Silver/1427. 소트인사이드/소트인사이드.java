0b118ac17871c14bde72530930129978ddeb25c4
hy <75581904+dhgkdud12@users.noreply.github.com>
2022-06-24 22:51:41 +0900

    [Silver V] Title: 소트인사이드, Time: 200 ms, Memory: 17736 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,23 +1,20 @@
 import java.awt.print.Pageable;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int m = sc.nextInt();
 
-        int n = m;
-        ArrayList<Integer> arrayList = new ArrayList<>();
-        while (n>0) {
-            arrayList.add(n%10);
-            n/=10;
-        }
-        Collections.sort(arrayList, Collections.reverseOrder());
-        for (Integer a : arrayList) {
-            System.out.print(a);
+        char[] arr = sc.nextLine().toCharArray();
+
+        Arrays.sort(arr);
+
+        for (int i= arr.length-1; i>=0; i--) {
+            System.out.print(arr[i]);
         }
     }
 }
